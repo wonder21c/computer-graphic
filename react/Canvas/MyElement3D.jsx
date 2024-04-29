58e563486fe5338585d4bba7bfc9bfad3943376b
손정우 <50861700+wonder21c@users.noreply.github.com>
2024-04-29 16:10:14 +0900

    Add files via upload

---
+++ react/Canvas/MyElement3D.jsx
@@ -0,0 +1,21 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
+function MyElement3D() {
+    const refMesh = useRef()
+    useFrame((state,delta) => {
+        refMesh.current.rotation.y += delta
+    }) 
+    return(
+        <>
+
+            <directionalLight position={(1,1,1)} />
+            <mesh ref = {refMesh} rotation-y = {45*Math.PI/180,0}>
+                <boxGeometry />
+                <meshStandardMaterial color= "#e67e22" />
+            </mesh>
+        </>
+    )
+}
+
+    export default MyElement3D
